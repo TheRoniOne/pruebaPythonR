7b3c160653c1049fcf1d241972d52e4312da95cc
TheRoniOne <42548202+TheRoniOne@users.noreply.github.com>
2020-04-08 15:58:51 -0500

    limpiado folder

--- algo.py
+++
@@ -1,8 +0,0 @@
-import json
-
-with open("telecom.json", "r") as file:
-    requestJSON = json.load(file)
-
-print(requestJSON)
-with open("miRequest.json", "w", encoding= "utf8") as file:
-    json.dump(requestJSON, file)
