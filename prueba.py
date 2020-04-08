a91c34287f790aa5e7b90a398013363bbe748ede
TheRoniOne <42548202+TheRoniOne@users.noreply.github.com>
2020-04-08 13:03:21 -0500

    empezado con flask

--- prueba.py
+++ prueba.py
@@ -1,10 +1,14 @@
 import sys, subprocess
+from flask import flask Flask, request, jsonify
 
 locJSON = sys.argv[1]
 nombreJSON = sys.argv[2]
 locResultado = sys.argv[3]
 
 comandoR = 'Rscript "D:/Datos de Usuario/rgamezv/Downloads/prueba/prueba.R" "{}" "{}" "{}"'.format(locJSON, nombreJSON, locResultado)
+
+def calcularResultado_API():
+
 
 def main():
     print(comandoR)
